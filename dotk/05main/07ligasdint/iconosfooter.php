090a99f1f7e1c8e4473159f6b4b968d5b81765f3
dlunna <dlunna@gmail.com>
2021-01-23 15:29:42 -0600

    Cambios de rutina

--- dotk/05main/07ligasdint/iconosfooter.php
+++ dotk/05main/07ligasdint/iconosfooter.php
@@ -15,75 +15,91 @@
           <div class="row justify-content-md-center" > <!-- primer bloque de 4 -->
 
             <!-- UNO -->
-              <a href="http://search.ebscohost.com">
-                <!--<p>- EBSCO - </p>-->
-                <img src="./images/ligas/ebsco.jpg" alt="ebsco search" class="img-thumbnail" >
-              </a>
+            <a href="http://www.upp.edu.mx/contraloriasocial/">
+              <!--<p>- Contraloría social - </p>-->
+              <img src="./images/ligas/contraloria_social.jpg" alt="Contraloria social" class="img-thumbnail">
+            </a>
+
             <!-- DOS -->
+            <a href="http://www.upp.edu.mx/fondosextraordinarios/">
+              <!--<p>- Fondos extraordinarios - </p>-->
+              <img src="./images/ligas/fondosextraordinarios.jpg" alt="fondos extraordinarios" class="img-thumbnail">
+            </a>
 
-              <a href="http://www.upp.edu.mx/contraloriasocial/">
-                <!--<p>- Contraloría social - </p>-->
-                <img src="./images/ligas/contraloria_social.jpg" alt="Contraloria social" class="img-thumbnail">
-              </a>
             <!-- TRES -->
+            <a href="http://s-contraloria.hidalgo.gob.mx/descargables/CODIGO-3.pdf">
+              <!--<p>- Código de Ética - </p>-->
+              <img src="./images/ligas/etica.jpg" alt="Cuadernillo de etica" class="img-thumbnail">
+            </a>
 
-              <a href="https://www.gob.mx/imjuve">
-                <!--<p>- IMJUVE - </p>-->
-                <img src="./images/ligas/imjuve.jpg" alt="tres" class="img-thumbnail">
-              </a>
             <!-- CUATRO -->
 
-              <a href="http://www.inegi.org.mx/default.aspx">
-                <!--<p>- INEGI - </p>-->
-                <img src="./images/ligas/inegi.jpg" alt="cuatro" class="img-thumbnail">
-              </a>
+
           </div> <!-- fin primer bloque de 4 -->
         </div>  <!-- fin primer bloque de 4 -->
         <!-- /Slide1 -->
 
-        <!-- /Slide2 -->
 
+
+        <!-- /Slide2 -->
         <div class="carousel-item">
           <div class="row justify-content-md-center">
-
-                  <a href="http://www.upp.edu.mx/fondosextraordinarios/">
-                    <!--<p>- Fondos extraordinarios - </p>-->
-                    <img src="./images/ligas/fondosextraordinarios.jpg" alt="fondos extraordinarios" class="img-thumbnail">
-                  </a>
 
                   <a href="http://www.unlugarparati.mx">
                     <!--<p>- Un lugar - </p>-->
                     <img src="./images/ligas/un-lugar-para-ti.jpg" alt="Un lugar para ti" class="img-thumbnail">
                   </a>
 
-                  <a href="http://s-contraloria.hidalgo.gob.mx/descargables/CODIGO-3.pdf">
-                    <!--<p>- Código de Ética - </p>-->
-                    <img src="./images/ligas/etica.jpg" alt="Cuadernillo de etica" class="img-thumbnail">
-
-                  </a>
-
-                  <a href="http://www.bibliotecaecest.mx">
-                    <!--<p>- ECEST - </p>-->
-
-                    <img src="./images/ligas/ecest.jpg" alt="ocho" class="img-thumbnail">
-
-                  </a>
-
                   <a href="http://ihfes.hidalgo.gob.mx/">
                     <!--<p>- IHFES - </p>-->
-
                     <img src="./images/ligas/ihfes.jpg" alt="nueve" class="img-thumbnail">
-
                   </a>
 
+                  <a href="https://www.gob.mx/imjuve">
+                    <!--<p>- IMJUVE - </p>-->
+                    <img src="./images/ligas/imjuve.jpg" alt="tres" class="img-thumbnail">
+                  </a>
+
+                  <a href="http://www.inegi.org.mx/default.aspx">
+                    <!--<p>- INEGI - </p>-->
+                    <img src="./images/ligas/inegi.jpg" alt="cuatro" class="img-thumbnail">
+                  </a>
 
           </div>
         </div>
-
         <!-- /Slide2 -->
 
 
+        <!-- /Slide3 -->
+        <div class="carousel-item">
+          <div class="row justify-content-md-center">
+
+                  <a href="https://search.ebscohost.com/login.aspx?authtype=custuid&custid=ns056256">
+                    <!--<p>- EBSCO - </p>-->
+                    <img src="./images/ligas/ebsco.jpg" alt="ebsco search" class="img-thumbnail" >
+                  </a>
+
+                  <a href="http://acceso.conricyt.mx/">
+                    <!--<p>- CONRICYT - </p>-->
+                    <img src="./images/ligas/conrycit.jpg" alt="conrycit search" class="img-thumbnail" >
+                  </a>
+
+                  <a href="https://web.b.ebscohost.com/ehost/search/basic?sid=abc6fc54-a39a-48c2-9261-47ca96d0d159@pdc-v-sessmgr04&vid=0&tid=2003EB&sdb=nlebk">
+                    <!--<p>- CONRICYT - </p>-->
+                    <img src="./images/ligas/InterfaceLogos.jpg" alt="300 mil libros" class="img-thumbnail" >
+                  </a>
+
+          </div>
+        </div>
+        <!-- /Slide3 -->
+
+
+
+
       </div> <!-- carousel inner -->
+
+
+
       <!-- /.control-box -->
 <!--
         <ul class="control-box pager">
@@ -100,6 +116,8 @@
         </ul>
 -->
       <!-- /.control-box -->
+
+
     </div><!-- /#eventosimp -->
 
   </div><!-- /.col-xs-12 -->
