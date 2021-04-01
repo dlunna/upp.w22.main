d9a0f04c664d8cb01b8c5c5efe5829e658986058
dlunna <dlunna@gmail.com>
2021-03-31 21:32:47 -0600

    Cambios por veda electoral y titulación

--- dotk/05main/07ligasdint/iconosfooter.php
+++ dotk/05main/07ligasdint/iconosfooter.php
@@ -16,20 +16,20 @@
 
             <!-- UNO -->
             <a href="http://www.upp.edu.mx/contraloriasocial/">
-              <!--<p>- Contraloría social - </p>-->
-              <img src="./images/ligas/contraloria_social.jpg" alt="Contraloria social" class="img-thumbnail">
+              <p>- Contraloría social - </p>
+              <!--<img src="./images/ligas/contraloria_social.jpg" alt="Contraloria social" class="img-thumbnail">-->
             </a>
 
             <!-- DOS -->
             <a href="http://www.upp.edu.mx/fondosextraordinarios/">
-              <!--<p>- Fondos extraordinarios - </p>-->
-              <img src="./images/ligas/fondosextraordinarios.jpg" alt="fondos extraordinarios" class="img-thumbnail">
+              <p>- Fondos extraordinarios - </p>
+              <!--<img src="./images/ligas/fondosextraordinarios.jpg" alt="fondos extraordinarios" class="img-thumbnail">-->
             </a>
 
             <!-- TRES -->
             <a href="http://s-contraloria.hidalgo.gob.mx/descargables/CODIGO-3.pdf">
-              <!--<p>- Código de Ética - </p>-->
-              <img src="./images/ligas/etica.jpg" alt="Cuadernillo de etica" class="img-thumbnail">
+              <p>- Código de Ética - </p>
+              <!--<img src="./images/ligas/etica.jpg" alt="Cuadernillo de etica" class="img-thumbnail">-->
             </a>
 
             <!-- CUATRO -->
@@ -46,23 +46,23 @@
           <div class="row justify-content-md-center">
 
                   <a href="http://www.unlugarparati.mx">
-                    <!--<p>- Un lugar - </p>-->
-                    <img src="./images/ligas/un-lugar-para-ti.jpg" alt="Un lugar para ti" class="img-thumbnail">
+                    <p>- Un lugar - </p>
+                    <!--<img src="./images/ligas/un-lugar-para-ti.jpg" alt="Un lugar para ti" class="img-thumbnail">-->
                   </a>
 
                   <a href="http://ihfes.hidalgo.gob.mx/">
-                    <!--<p>- IHFES - </p>-->
-                    <img src="./images/ligas/ihfes.jpg" alt="nueve" class="img-thumbnail">
+                    <p>- IHFES - </p>
+                    <!--<img src="./images/ligas/ihfes.jpg" alt="nueve" class="img-thumbnail">-->
                   </a>
 
                   <a href="https://www.gob.mx/imjuve">
-                    <!--<p>- IMJUVE - </p>-->
-                    <img src="./images/ligas/imjuve.jpg" alt="tres" class="img-thumbnail">
+                    <p>- IMJUVE - </p>
+                    <!--<img src="./images/ligas/imjuve.jpg" alt="tres" class="img-thumbnail">-->
                   </a>
 
                   <a href="http://www.inegi.org.mx/default.aspx">
-                    <!--<p>- INEGI - </p>-->
-                    <img src="./images/ligas/inegi.jpg" alt="cuatro" class="img-thumbnail">
+                    <p>- INEGI - </p>
+                    <!--<img src="./images/ligas/inegi.jpg" alt="cuatro" class="img-thumbnail">-->
                   </a>
 
           </div>
@@ -75,18 +75,18 @@
           <div class="row justify-content-md-center">
 
                   <a href="https://search.ebscohost.com/login.aspx?authtype=custuid&custid=ns056256">
-                    <!--<p>- EBSCO - </p>-->
-                    <img src="./images/ligas/ebsco.jpg" alt="ebsco search" class="img-thumbnail" >
+                    <p>- EBSCO Host - </p>
+                    <!--<img src="./images/ligas/ebsco.jpg" alt="ebsco search" class="img-thumbnail" >-->
                   </a>
 
                   <a href="http://acceso.conricyt.mx/">
-                    <!--<p>- CONRICYT - </p>-->
-                    <img src="./images/ligas/conrycit.jpg" alt="conrycit search" class="img-thumbnail" >
+                    <p>- CONRICYT - </p>
+                    <!--<img src="./images/ligas/conrycit.jpg" alt="conrycit search" class="img-thumbnail" >-->
                   </a>
 
                   <a href="https://web.b.ebscohost.com/ehost/search/basic?sid=abc6fc54-a39a-48c2-9261-47ca96d0d159@pdc-v-sessmgr04&vid=0&tid=2003EB&sdb=nlebk">
-                    <!--<p>- CONRICYT - </p>-->
-                    <img src="./images/ligas/InterfaceLogos.jpg" alt="300 mil libros" class="img-thumbnail" >
+                    <p>- EBSCO Publishing - </p>
+                    <!--<img src="./images/ligas/InterfaceLogos.jpg" alt="300 mil libros" class="img-thumbnail" >-->
                   </a>
 
           </div>
